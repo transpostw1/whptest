7b132b2a3c7c219dba1fcb5306fab73b5b7eaeed
Rutuja <rutujaparab@transpost.co>
2024-03-27 10:52:18 +0530

    Fixed Banner

--- src/components/Slider/MainCarousel.tsx
+++ src/components/Slider/MainCarousel.tsx
@@ -12,7 +12,7 @@ const MainCarousel = () => {
   return (
     <>
       <div className="slider-block style-one bg-linear 2xl:h-[500px] xl:h-[622px] lg:h-[562] md:h-[462px] sm:h-[382px] h-[302px] w-full">
-        <div className="slider-main h-full w-full">
+        <div className="slider-main w-full h-full" >
           <Swiper
             spaceBetween={0}
             slidesPerView={1}
@@ -25,58 +25,47 @@ const MainCarousel = () => {
             }}
           >
             <SwiperSlide>
-              <div className="slider-item h-full w-full relative">
-                <div className="container w-full h-full flex items-center">
-                  <div className="sub-img absolute left-0 top-0 w-full h-full z-[-1]">
-                    <Image
-                      src={"/images/slider/SliderTwo.png"}
-                      width={2560}
-                      height={1080}
-                      alt="1920x820"
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                </div>
+              <div className="slider-item h-full w-full">
+                <Image
+                  src={"/images/slider/SliderTwo.png"}
+                  width={2560}
+                  height={1080}
+                  alt="1920x820"
+                  className="w-full h-full object-fill"
+                />
               </div>
             </SwiperSlide>
-            {/* <SwiperSlide>
-              <div className="slider-item h-full w-full relative">
-                <div className="container w-full h-full flex items-center">
-                  <div className="sub-img absolute left-0 top-0 w-full h-full z-[-1]">
-                    <video
-                      className="w-full h-full object-cover"
-                      autoPlay
-                      loop
-                      muted
-                    >
-                      <source
-                        src="/images/other/banner_video_4.mp4"
-                        type="video/mp4"
-                      />
-                      Your browser does not support the video tag.
-                    </video>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide> */}
             <SwiperSlide>
-              <div className="slider-item h-full w-full relative">
-                <div className="container w-full h-full flex items-center">
-                  <div className="sub-img absolute left-0 top-0 w-full h-full z-[-1]">
-                    <video
-                      className="w-full h-full object-cover"
-                      autoPlay
-                      loop
-                      muted
-                    >
-                      <source
-                        src="/images/other/banner_video_3.mp4"
-                        type="video/mp4"
-                      />
-                      Your browser does not support the video tag.
-                    </video>
-                  </div>
-                </div>
+              <div className="slider-item h-full w-full ">
+                <video
+                  className="w-full h-full object-fill"
+                  autoPlay
+                  loop
+                  muted
+                >
+                  <source
+                    src="/images/other/banner_video_4.mp4"
+                    type="video/mp4"
+                  />
+                  Your browser does not support the video tag.
+                </video>
+
+              </div>
+            </SwiperSlide> 
+            <SwiperSlide>
+              <div className="slider-item h-full w-full ">
+                <video
+                  className="w-full h-full object-fill"
+                  autoPlay
+                  loop
+                  muted
+                >
+                  <source
+                    src="/images/other/banner_video_3.mp4"
+                    type="video/mp4"
+                  />
+                  Your browser does not support the video tag.
+                </video>
               </div>
             </SwiperSlide>
           </Swiper>

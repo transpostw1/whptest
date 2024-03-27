0436f314653e097a8c4e5fbb14c739e99e74a9ee
Rutuja <rutujaparab@transpost.co>
2024-03-27 16:59:33 +0530

    What We Offer UI Changes

--- src/components/Home1/WhatWeOffer.tsx
+++ src/components/Home1/WhatWeOffer.tsx
@@ -12,14 +12,14 @@ const WhatWeOffer = () => {
                     <div className='w-full flex flex-col sm:w-[50%] p-4 sm:p-[4rem]'>
                         <div>
                             <p className='lg:text-[3rem] text-[2rem] font-semibold pb-5'>What we Offer</p>
-                            <p className='text-sm md:text-base'>Discover our offerings. Our commitment to quality, elegance, and personalised service ensures an exceptional experience.</p>
+                            <p className='text-sm md:text-base w-[30rem]'>Discover our offerings. Our commitment to quality, elegance, and personalised service ensures an exceptional experience.</p>
                         </div>
                         <div className='mt-auto flex-end'>
                             <div className='flex justify-between'>
                                 <div>
                                     <Image src={'/images/icons/star.svg'} alt='Star' width={40} height={40} />
                                     <p className='font-semibold text-lg md:text-xl py-3'>12000 + UNIQUE DESIGNS</p>
-                                    <p className='text-sm md:text-base'>Elevate your style with our distinctive jewellery designs, where creativity and craftsmanship unite.</p>
+                                    <p className='text-sm md:text-base w-[30rem]'>Elevate your style with our distinctive jewellery designs, where creativity and craftsmanship unite.</p>
                                     <a className='pt-3 inline-flex items-center'><span className='me-2 text-[#E26178] underline cursor-pointer text-sm'>Next</span><span><Image src={'/images/icons/rightarrow.svg'} alt='Right Arrow' width={20} height={20} /></span></a>
                                 </div>
                                 <div className="hidden sm:block">

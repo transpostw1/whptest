b2f7da019f5fe5815e8c044586e4f9e2fff55a69
masoodw1 <masoodahmed@transpost.co>
2024-03-28 15:22:30 +0530

    test

--- src/components/Home1/WhpTv.tsx
+++ src/components/Home1/WhpTv.tsx
@@ -3,6 +3,7 @@
 import React ,{useState, useEffect}from "react";
 import Image from "next/image";
 import Link from "next/link";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper/modules";
 import "swiper/css/bundle";

9f3bdf783c490d3b1605e1ab40d337a2ff09fb09
AadityaTelange <aadityatelange@tranpost.com>
2024-04-21 18:24:26 +0530

    updated

---
+++ src/app/login/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='fixed inset-0 z-50 flex bg-black bg-opacity-50 h-full'>loading...</div>
+  )
+}
+
+export default loading

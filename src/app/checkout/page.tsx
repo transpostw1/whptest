7093fcbfd27c72766f7c98941e0722589afc8512
Rutuja <rutujaparab@transpost.co>
2024-05-27 15:06:26 +0530

    Fixed checkout

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -128,12 +128,8 @@ const Checkout: React.FC = () => {
           }
         );
         setDataAfterCouponCode(response.data);
-        setFlashMessage("Coupon Successfully applied");
-        setFlashType("success");
-      } catch (error:any) {
+      } catch (error) {
         console.log("Error occurred", error);
-        setFlashMessage(error.response.data.message);
-        setFlashType("error");
       } finally {
         setLoading(false);
       }
@@ -207,7 +203,7 @@ const Checkout: React.FC = () => {
       price: item?.productDetails?.discountPrice,
       image:
         item?.productDetails?.imageDetails &&
-        item?.productDetails?.imageDetails.length > 0
+          item?.productDetails?.imageDetails.length > 0
           ? item.productDetails.imageDetails[0].image_path
           : "",
     }));
@@ -406,9 +402,8 @@ const Checkout: React.FC = () => {
     {
       icon: (
         <ShoppingCart
-          className={`text-2xl rounded-full ${
-            selectedStep === 0 ? "text-white" : "text-white"
-          }`}
+          className={`text-2xl rounded-full ${selectedStep === 0 ? "text-white" : "text-white"
+            }`}
         />
       ),
       label: "Cart",
@@ -416,9 +411,8 @@ const Checkout: React.FC = () => {
     {
       icon: (
         <Icon.MapPin
-          className={`text-2xl text-black ${
-            selectedStep === 1 || selectedStep === 2 ? "text-white" : ""
-          }`}
+          className={`text-2xl text-black ${selectedStep === 1 || selectedStep === 2 ? "text-white" : ""
+            }`}
         />
       ),
       label: "Address",
@@ -426,9 +420,8 @@ const Checkout: React.FC = () => {
     {
       icon: (
         <Wallet
-          className={`text-2xl  ${
-            selectedStep === 2 ? "text-white" : "text-black"
-          }`}
+          className={`text-2xl  ${selectedStep === 2 ? "text-white" : "text-black"
+            }`}
         />
       ),
       label: "Payment",
@@ -444,138 +437,99 @@ const Checkout: React.FC = () => {
   return (
     <>
       {/* <ProtectedRoute> */}
-        <div className="cart-block flex-wrap mb-8">
-          <div className="content-main flex flex-col justify-between lg:px-14 px-5">
-            <div className="flex w-full justify-between items-center bg-[#F8F3F466]">
-              <div className="flex gap-3">
-                {isOrderPlaced ? (
-                  <div className="flex">
-                    <Image
-                      src="/images/collection/check.png"
-                      alt="check"
-                      width={100}
-                      height={10}
-                    />
-                    <div className="flex flex-col items-start justify-center py-3 ">
-                      <h1 className="text-3xl text-red-700 font-semibold">
-                        Order Placed!!
-                      </h1>
-                      <h1>ID-#32432</h1>
-                    </div>
+      <div className="cart-block flex-wrap mb-8">
+        <div className="content-main flex flex-col justify-between lg:px-14 px-5">
+          <div className="flex w-full justify-between items-center bg-[#F8F3F466]">
+            <div className="flex gap-3">
+              {isOrderPlaced ? (
+                <div className="flex">
+                  <Image
+                    src="/images/collection/check.png"
+                    alt="check"
+                    width={100}
+                    height={10}
+                  />
+                  <div className="flex flex-col items-start justify-center py-3 ">
+                    <h1 className="text-3xl text-red-700 font-semibold">
+                      Order Placed!!
+                    </h1>
+                    <h1>ID-#32432</h1>
                   </div>
-                ) : (
-                  <div className="flex mt-2 items-center sm:mr-1 lg:mr-3 p-2 w-full">
-                    {steps.map((step, index) => (
+                </div>
+              ) : (
+                <div className="flex mt-2 items-center sm:mr-1 lg:mr-3 p-2 w-full">
+                  {steps.map((step, index) => (
+                    <div
+                      className="flex items-center lg:w-40 max-sm:w-25 max-md:w-25"
+                      key={index}
+                      onClick={() =>
+                        handleStepClick(index, useSameAsBillingAddress)
+                      }
+                    >
                       <div
-                        className="flex items-center lg:w-40 max-sm:w-25 max-md:w-25"
-                        key={index}
-                        onClick={() =>
-                          handleStepClick(index, useSameAsBillingAddress)
-                        }
-                      >
-                        <div
-                          className={`p-2 rounded-full border border-gray-300 ${
-                            selectedStep >= index ? "bg-rose-400" : "bg-white"
+                        className={`p-2 rounded-full border border-gray-300 ${selectedStep >= index ? "bg-rose-400" : "bg-white"
                           }`}
-                        >
-                          {step.icon}
-                        </div>
-                        <h2 className="rounded-full cursor-pointer">
-                          {step.label}
-                        </h2>
-                        {index < steps.length - 1 && (
-                          <Icon.CaretRight className="sm:mr-0 sm:ml-0 lg:mr-[10px] lg:ml-[10px]" />
-                        )}
+                      >
+                        {step.icon}
                       </div>
-                    ))}
-                  </div>
-                )}
-              </div>
+                      <h2 className="rounded-full cursor-pointer">
+                        {step.label}
+                      </h2>
+                      {index < steps.length - 1 && (
+                        <Icon.CaretRight className="sm:mr-0 sm:ml-0 lg:mr-[10px] lg:ml-[10px]" />
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
-            {!isOrderPlaced ? (
-              <h2>(Review of {cartItems.length} Items)</h2>
-            ) : null}
-            <FlashAlert
-              key={flashKey}
-              message={flashMessage}
-              type={flashType}
-            />
-            <div className="flex flex-col md:flex-row lg:flex-row justify-between">
-              <div className="w-full md:w-[2000px] sm:mt-7 mt-5 md:pr-5">
-                <div className="heading bg-surface bora-4 pt-4 pb-4"></div>
-                {selectedComponent === "CartItems" && (
-                  <CartItems
-                    cartItems={mappedCartItems}
-                    handleQuantityChange={handleQuantityChange}
-                    removeFromCart={removeFromCart}
-                  />
-                )}
-                {selectedComponent === "DeliveryDetails" && (
-                  <DeliveryDetails
-                    onShippingAddressSelected={onShippingAddressSelected}
-                    onBillingAddressSelected={onBillingAddressSelected}
-                    useSameAsBillingAddress={useSameAsBillingAddress}
-                    setUseSameAsBillingAddress={setUseSameAsBillingAddress}
-                    selectedShippingAddress={selectedShippingAddress}
-                    setSelectedShippingAddress={setSelectedShippingAddress}
-                    selectedBillingAddress={selectedBillingAddress}
-                    setSelectedBillingAddress={setSelectedBillingAddress}
-                  />
-                )}
-                {selectedComponent === "Payment" && (
-                  <Payment
-                    orderPlaced={isOrderPlaced}
-                    selectedPaymentMethod={selectedPaymentMethod}
-                    handlePaymentMethodChange={handlePaymentMethodChange}
-                    totalCart={totalCart}
-                    onOrderComplete={handleOrderComplete}
-                    selectedShippingAddress={selectedShippingAddress}
-                    selectedBillingAddress={selectedBillingAddress}
-                    placeOrder={handleProceed}
-                    mappedCartItems={mappedCartItems}
-                    couponCode={couponCode}
-                    totalDiscount={totalDiscount}
-                    setCartItems={setCartItems}
-                  />
-                )}
-            <div className="flex flex-col md:flex-row lg:flex-row justify-between">
-              <div className="w-full md:w-[2000px] sm:mt-7 mt-5 md:pr-5">
-                <div className="heading bg-surface bora-4 pt-4 pb-4"></div>
-                {selectedComponent === "CartItems" && (
-                  <CartItems
-                    cartItems={mappedCartItems}
-                    handleQuantityChange={handleQuantityChange}
-                    removeFromCart={removeFromCart}
-                  />
-                )}
-                {selectedComponent === "DeliveryDetails" && (
-                  <DeliveryDetails
-                    onShippingAddressSelected={onShippingAddressSelected}
-                    onBillingAddressSelected={onBillingAddressSelected}
-                    useSameAsBillingAddress={useSameAsBillingAddress}
-                    setUseSameAsBillingAddress={setUseSameAsBillingAddress}
-                    selectedShippingAddress={selectedShippingAddress}
-                    setSelectedShippingAddress={setSelectedShippingAddress}
-                    selectedBillingAddress={selectedBillingAddress}
-                    setSelectedBillingAddress={setSelectedBillingAddress}
-                  />
-                )}
-                {selectedComponent === "Payment" && (
-                  <Payment
-                    orderPlaced={isOrderPlaced}
-                    selectedPaymentMethod={selectedPaymentMethod}
-                    handlePaymentMethodChange={handlePaymentMethodChange}
-                    totalCart={totalCart}
-                    onOrderComplete={handleOrderComplete}
-                    selectedShippingAddress={selectedShippingAddress}
-                    selectedBillingAddress={selectedBillingAddress}
-                    placeOrder={handleProceed}
-                    mappedCartItems={mappedCartItems}
-                    couponCode={couponCode}
-                    totalDiscount={totalDiscount}
-                    setCartItems={setCartItems}
-                  />
-                )}
+          </div>
+          {!isOrderPlaced ? (
+            <h2>(Review of {cartItems.length} Items)</h2>
+          ) : null}
+          <FlashAlert
+            key={flashKey}
+            message={flashMessage}
+            type={flashType}
+          />
+          <div className="flex flex-col md:flex-row lg:flex-row justify-between">
+            <div className="w-full md:w-[2000px] sm:mt-7 mt-5 md:pr-5">
+              <div className="heading bg-surface bora-4 pt-4 pb-4"></div>
+              {selectedComponent === "CartItems" && (
+                <CartItems
+                  cartItems={mappedCartItems}
+                  handleQuantityChange={handleQuantityChange}
+                  removeFromCart={removeFromCart}
+                />
+              )}
+              {selectedComponent === "DeliveryDetails" && (
+                <DeliveryDetails
+                  onShippingAddressSelected={onShippingAddressSelected}
+                  onBillingAddressSelected={onBillingAddressSelected}
+                  useSameAsBillingAddress={useSameAsBillingAddress}
+                  setUseSameAsBillingAddress={setUseSameAsBillingAddress}
+                  selectedShippingAddress={selectedShippingAddress}
+                  setSelectedShippingAddress={setSelectedShippingAddress}
+                  selectedBillingAddress={selectedBillingAddress}
+                  setSelectedBillingAddress={setSelectedBillingAddress}
+                />
+              )}
+              {selectedComponent === "Payment" && (
+                <Payment
+                  orderPlaced={isOrderPlaced}
+                  selectedPaymentMethod={selectedPaymentMethod}
+                  handlePaymentMethodChange={handlePaymentMethodChange}
+                  totalCart={totalCart}
+                  onOrderComplete={handleOrderComplete}
+                  selectedShippingAddress={selectedShippingAddress}
+                  selectedBillingAddress={selectedBillingAddress}
+                  placeOrder={handleProceed}
+                  mappedCartItems={mappedCartItems}
+                  couponCode={couponCode}
+                  totalDiscount={totalDiscount}
+                  setCartItems={setCartItems}
+                />
+              )}
 
               {/* <h3 className="font-medium">Estimated Delivery Date:29/2/2024</h3> */}
             </div>
@@ -682,64 +636,9 @@ const Checkout: React.FC = () => {
                   )}
                 </div>
               )}
-                    </div>
-                    <h3
-                      className="text-red-600 underline cursor-pointer"
-                      onClick={() => handleGiftWrapModal()}
-                    >
-                      Add
-                    </h3>
-                    {showModal && (
-                      <GiftWrapModal closeModal={handleCloseModal} />
-                    )}
-                  </div>
-                  <p className="mt-2 font-bold text-lg">ORDER SUMMARY</p>
-                  {loading ? (
-                    <Skeleton height={150} />
-                  ) : (
-                    <div className="bg-gray-100 p-2 mt-2">
-                      <div className="">
-                        <div className="flex justify-between font-medium">
-                          <h3>Subtotal</h3>
-                          <h3>
-                            ₹
-                            {Intl.NumberFormat("en-IN", {
-                              minimumFractionDigits: 2,
-                            }).format(Math.round(parseInt(formattedPrice)))}
-                          </h3>
-                        </div>
-                        <div className="flex justify-between font-medium">
-                          <h3>Discount</h3>
-                          <h3>
-                            ₹
-                            {Intl.NumberFormat("en-IN", {
-                              minimumFractionDigits: 2,
-                            }).format(Math.round(parseInt(totalDiscount)))}
-                          </h3>
-                        </div>
-                        <div className="flex justify-between font-medium">
-                          <h3>Shipping Charges</h3>
-                          <h3>₹0</h3>
-                        </div>
-                        <div className="flex justify-between font-bold">
-                          <h3 className="text-gray-800">Total Price</h3>
-                          <h3>
-                            ₹
-                            {Intl.NumberFormat("en-IN", {
-                              minimumFractionDigits: 2,
-                            }).format(
-                              Math.round(parseInt(totalPrice.toString()))
-                            )}
-                          </h3>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              )}
 
-                {(selectedComponent === "DeliveryDetails" ||
-                  selectedComponent === "Payment") && (
+              {(selectedComponent === "DeliveryDetails" ||
+                selectedComponent === "Payment") && (
                   <div>
                     <h1 className="my-5 text-2xl text-rose-600">
                       ORDER SUMMARY
@@ -765,9 +664,8 @@ const Checkout: React.FC = () => {
           </div>
         </div>
       </div>
-      {couponCode && (
-        <FlashAlert key={flashKey} message={flashMessage} type={flashType} />
-      )}
+      {/* </ProtectedRoute> */}
+
       {isMobile && (
         <div className="flex fixed bottom-0 bg-white w-full p-3 z-50 justify-between">
           <div>

31952346cea1369554e557b56a5ff447e61c484a
raamizz <rameezebrahim@transpost.co>
2024-06-05 12:21:25 +0530

    Blogs add updated with api

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import Latest from '@/components/Blogcomp/Latest'
-import Trending from '@/components/Blogcomp/Trending'
-import Card from '@/components/Blogcomp/Card'
-import instance from '@/utils/axios'
+"use client";
+import React, { useState, useEffect } from "react";
+import Latest from "@/components/Blogcomp/Latest";
+import Trending from "@/components/Blogcomp/Trending";
+import Card from "@/components/Blogcomp/Card";
+import axios from "axios";
+import { baseUrl,blogs } from "@/utils/constants";
 
-const page = () => {
+const Blogs = () => {
+  const [blogData, setBlogData] = useState([]);
+  useEffect(() => {
+    const fetchBlogData = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}${blogs}`);
+        setBlogData(response.data);
+      } catch (error) {
+        console.log("error fetching data of blog", error);
+      }
+    };
+    fetchBlogData();
+  }, []);
   return (
     <div>
-      <Latest/>
-      <Trending/>
-      <Card/>
+      <Latest blogData={blogData} />
+      <Trending blogData={blogData} />
+      <Card />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Blogs;

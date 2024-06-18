146622487e7ef05ed639df5aa800270a071b407c
Rutuja <rutujaparab@transpost.co>
2024-06-18 10:00:46 +0530

    Fixed wishlist

--- src/utils/cartUtils.ts
+++ src/utils/cartUtils.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
-import { baseUrl, getCartItems } from "./constants";
+import { baseUrl, getCartItems, graphqlbaseUrl } from "./constants";
 import Cookies from "js-cookie";
+import { ApolloClient, InMemoryCache, gql, HttpLink } from "@apollo/client";
+
 
 // interface CartItem {
 //   productId: number;
@@ -34,21 +36,75 @@ export const fetchCartItemsFromServer = async (): Promise<CartItem[]> => {
       return [];
     }
 
-    const response = await axios.get(`${baseUrl}${getCartItems}`, {
-      headers: {
-        Authorization: `Bearer ${userToken}`,
-      },
+    const getAuthHeaders = () => {
+      if (!userToken) return null;
+      return {
+        authorization: `Bearer ${userToken}`,
+      };
+    };
+
+    const link = new HttpLink({
+      uri: graphqlbaseUrl,
+      headers: getAuthHeaders(),
     });
 
-    const cartItemsData = response.data.cart_items.map((item: any) => {
-      const imageDetails = JSON.parse(item.product_details[0].imageDetails);
+    const client = new ApolloClient({
+      link,
+      cache: new InMemoryCache(),
+    });
+
+    const GET_CART = gql `query Query($token: String!) {
+      getCustomerCart(token: $token) {
+        id
+        quantity
+        productDetails {
+          productId
+          productAmount
+          quantity
+          url
+          SKU
+          variantId
+          productTotal
+          metalType
+          metalWeight
+          discountAmount
+          discountValue
+          typeOfDiscount
+          discountedTotal
+          displayTitle
+          productPrice
+          discountPrice
+          mediaId
+          imageDetails {
+            image_path
+            order
+            alt_text
+          }
+          videoDetails {
+            video_path
+            order
+            alt_text
+          }
+          rating
+        }
+      }
+    }`;
+
+    const variables = { token: userToken };
+        const { data } = await client.query({
+          query: GET_CART,
+          variables,
+        });
+
+    const cartItemsData = data.map((item: any) => {
+      const imageDetails = JSON.parse(item.productDetails[0].imageDetails);
       imageDetails.sort((a: any, b: any) => a.order - b.order);
       const imagePath = imageDetails[0] ? imageDetails[0].image_path : "";
       return {
         productId: item.productId,
         quantity: item.quantity,
-        name: item.product_details[0].displayTitle,
-        price: parseInt(item.product_details[0].discountPrice),
+        name: item.productDetails[0].displayTitle,
+        price: parseInt(item.productDetails[0].discountPrice),
         image: imagePath,
       };
     });

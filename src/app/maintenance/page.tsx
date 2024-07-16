d39b6156f3723ed6546fcaa2141e3cdd22f7f5f7
raamizz <rameezebrahim@transpost.co>
2024-07-16 12:22:01 +0530

    Maintanance update

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,25 +1,7 @@
-"use client"
-import React,{useEffect} from 'react'
-import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
+import React from 'react'
 
 const Maintanance = () => {
     
-  const router = useRouter();
-  const pathname = usePathname()
-
-  useEffect(() => {
-    if (
-      process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true' &&
-      pathname !== '/maintenance'
-    ) {
-      router.push('/maintenance');
-    }
-  }, [pathname,router]);
-
-  if (process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true' && pathname !== '/maintenance') {
-    return null;
-  }
   return (
 
     <div style={{ textAlign: 'center', marginTop: '20%' }}>

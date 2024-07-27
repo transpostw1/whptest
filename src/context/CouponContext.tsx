72d27783db9b246f0b9533b9b89a19c85e75d47b
Rutuja <rutujaparab@transpost.co>
2024-07-27 11:03:40 +0530

    merged from aaditya

--- src/context/CouponContext.tsx
+++ src/context/CouponContext.tsx
@@ -1,84 +1,34 @@
 "use client";
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { ApolloClient, InMemoryCache, gql, HttpLink } from "@apollo/client";
-import { graphqlbaseUrl } from "@/utils/constants";
-
-interface Coupon {
-  id: string;
-  name: string;
-  code: string;
-  discountOn: string;
-  discountType: string;
-  discountValue: number;
-  discountMinAmount: number;
-  discountMaxAmount: number;
-  discountStartDate: string;
-  discountEndDate: string;
-}
-
 interface CouponContextProps {
-  totalDiscount: number;
-  updateDiscount: (discount: number) => void;
-  coupons: Coupon[];
-  fetchCoupons: () => void;
+  totalDiscount: number | any;
+  updateDiscount: (discount: any) => void;
+  updateTotalDiscount: (discount: any) => void;
+  // setTotalDiscount: React.Dispatch<React.SetStateAction<number>>;
 }
-
 
 const CouponContext = createContext<CouponContextProps | undefined>(undefined);
-
-const GET_COUPONS = gql`
-query GetAllCoupons {
-  getAllCoupons {
-    id
-    name
-    code
-    discountOn
-    discountType
-    discountValue
-    discountMinAmount
-    discountMaxAmount
-    discountStartDate
-    discountEndDate
-  }
-}
-`;
 
 export const CouponCodeProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [totalDiscount, setTotalDiscount] = useState<number>(0);
   const [resetDiscount, setResetDiscount] = useState<boolean>(false);
-  const [coupons, setCoupons] = useState<Coupon[]>([]);
 
   const updateDiscount = (discount: any) => {
     setTotalDiscount(discount);
   };
-
-  const fetchCoupons = async () => {
-    try {
-      const client = new ApolloClient({
-        uri: graphqlbaseUrl,
-        cache: new InMemoryCache(),
-      });
-
-      const { data } = await client.query({
-        query: GET_COUPONS,
-      });
-      const fetchedCoupons = data.getAllCoupons;
-      setCoupons(fetchedCoupons);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+  const updateTotalDiscount = (discount: any) => {
+    setTimeout(() => {
+      setTotalDiscount((prevTotalDiscount) => prevTotalDiscount);
+      setResetDiscount(true);
+    }, 1000);
   };
-
-  useEffect(() => {
-    fetchCoupons();
-  }, []);
 
   useEffect(() => {
     if (resetDiscount) {
       setTotalDiscount(0);
-      setResetDiscount(false); 
+      setResetDiscount(false); // Reset resetDiscount back to false
     }
   }, [resetDiscount]);
 

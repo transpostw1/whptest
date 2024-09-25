ac877e60fdbcb49563a417d5b467031ba6048cdc
Rutuja <rutujaparab@transpost.co>
2024-09-25 17:47:01 +0530

    removed loader unoptimzed

--- src/components/Other/Loader.tsx
+++ src/components/Other/Loader.tsx
@@ -6,7 +6,7 @@ const Loader = () => {
   return (
     <div className="backdrop fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center z-50">
       <div className="loading-container flex justify-center items-center h-full">
-        <Image src="/dummy/loader.gif" alt={"loader"} height={50} width={50} unoptimized/>
+        <Image src="/dummy/loader.gif" alt={"loader"} height={50} width={50}/>
       </div>
     </div>
   );

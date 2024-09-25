f549db11291aa68070852bf11f8e75a35e39f0fd
AadityaTelange <aadityatelange@tranpost.com>
2024-09-25 17:46:45 +0530

    fixed edit address

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   reactStrictMode: false,
+  productionBrowserSourceMaps: false,
   eslint: {
     ignoreDuringBuilds: true,
   },

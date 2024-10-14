eca441962b701e0b06b39641076a7da8166858cb
AadityaTelange <aadityatelange@tranpost.com>
2024-10-14 11:39:25 +0530

    add link to the product in order summary

--- src/app/checkout/OrderSummaryProducts.tsx
+++ src/app/checkout/OrderSummaryProducts.tsx
@@ -1,49 +1,54 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { useCurrency } from "@/context/CurrencyContext";
 interface OrderSummaryProductsProps {
   cartItems: any[];
   isBuyNow: boolean;
-
 }
 
 const OrderSummaryProducts: React.FC<OrderSummaryProductsProps> = ({
   cartItems,
   isBuyNow,
-
 }) => {
   const { formatPrice } = useCurrency();
   const displayedItems = isBuyNow ? cartItems.slice(0, 1) : cartItems;
 
-
   return (
-    <div className="list-product-main w-full hidden lg:block mb-2">
+    <div className="list-product-main mb-2 hidden w-full lg:block">
       {displayedItems?.length < 1 ? (
-        <p className="text-xl text-center font-medium pt-3">
+        <p className="pt-3 text-center text-xl font-medium">
           There is nothing here!
         </p>
       ) : (
         displayedItems?.map((product, index) => (
           <div
-            className="border border-gray-200 flex w-full items-center mb-2"
+            className="mb-2 flex w-full items-center border border-gray-200"
             key={index}
           >
-            <Image
-              src={product.image}
-              width={100}
-              height={200}
-              alt={product.name}
-              className="rounded-lg object-cover"
-              unoptimized
-            />
-            <div className="p-4 flex flex-col">
+            <Link href={`products/${product.productId}/${product.url}`}>
+              <Image
+                src={product.image}
+                width={100}
+                height={200}
+                alt={product.name}
+                className="rounded-lg object-cover"
+                unoptimized
+              />
+            </Link>
+            <div className="flex flex-col p-4">
               <div className="text-title">
-                {product.name} <span className="font-normal lowercase">x {product.quantity}</span>
+                <Link href={`products/${product.productId}/${product.url}`}>
+                  {product.name}
+                </Link>
+                <span className="font-normal lowercase">
+                  x {product.quantity}
+                </span>
               </div>
               <div className="text-title text-start">
                 {formatPrice(product.price)}
               </div>
-              <h3>Estimated Delivery Date: 29/2/2024</h3>
+              {/* <h3>Estimated Delivery Date: 29/2/2024</h3> */}
             </div>
           </div>
         ))

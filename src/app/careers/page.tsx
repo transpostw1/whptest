51e4e0ec62550c436a7c20c3d23c153c3308ca50
raamizz <rameezebrahim@transpost.co>
2024-10-31 12:19:57 +0530

    Career cursor

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -150,7 +150,7 @@ const Careers: React.FC = () => {
           {filteredcareersData.length > 0 ? (
             filteredcareersData.map((data, index) => (
               <div
-                className="mx-2 mb-3 rounded-2xl border border-[#bb547d] px-5 py-2 text-gray-600 shadow-xl md:p-7"
+                className="mx-2 mb-3 rounded-2xl border border-[#bb547d] px-5 py-2 text-gray-600 shadow-xl md:p-7 cursor-pointer"
                 key={index}
                 onClick={(event) => handleJobDetail(data.url, event)}
               >

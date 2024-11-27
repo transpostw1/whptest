e03149005e39908d86e9b7a7d462f13d12993e0a
raamizz <rameezebrahim@transpost.co>
2024-11-27 14:30:25 +0530

    Update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -92,123 +92,121 @@ const Register = () => {
   };
 
   return (
-    <>
-      <head>
-        <title>Signup</title>
-        <meta name="description" content={"Sign Up to WHP."} />
-      </head>
-      <div className="register-block py-10 md:py-20">
-        <div className="container">
-          <div className="flex justify-center max-md:flex-col">
-            <div className="border-line md:pr-[40px] lg:pr-[60px]">
-              <form onSubmit={formik.handleSubmit}>
-                <div className="heading4 text-center text-[#e26178]">
-                  <AuthAnimation />
-                  <h1 className="mb-5 text-center text-lg font-normal text-[#E26178]">
-                    SIGNUP TO WHP
-                  </h1>
-                </div>
-
-                {!isOtpVerified && (
-                  <div className="flex flex-col items-center">
-                    <PhoneInput
-                      defaultCountry="in"
-                      value={phoneNumber}
-                      inputClassName="border h-full w-[250px] rounded-xl"
-                      placeholder="Enter your mobile number"
-                      onChange={handlePhoneChange}
-                    />
-                    <div className="block-button mt-4 w-full md:mt-7">
-                      <div className="">
-                        <OtpVerification
-                          phoneNumber={phoneNumber}
-                          formikValues={formik.values}
-                          onSubmit={formik.handleSubmit}
-                          isRegisterPage={false}
-                          onOtpVerified={() => setIsOtpVerified(true)}
-                          errorMessage=""
-                        />
-                      </div>
-                    </div>
-                  </div>
-                )}
-
-                {isOtpVerified && (
-                  <form onSubmit={formik.handleSubmit} className="mt-4 md:mt-7">
-                    <div className="firstname">
-                      <label htmlFor="firstName" className="font-medium">
-                        First Name
-                      </label>
-                      <input
-                        className="w-full rounded-lg border border-gray-300 px-4 pb-3 pt-3"
-                        placeholder="First Name"
-                        id="firstName"
-                        type="text"
-                        {...formik.getFieldProps("firstName")}
-                        disabled={submitted}
-                      />
-                      {formik.touched.firstName && formik.errors.firstName ? (
-                        <div className="text-red-500">
-                          {formik.errors.firstName}
-                        </div>
-                      ) : null}
-                    </div>
-                    <div className="lastname mt-5">
-                      <label htmlFor="lastName" className="font-medium">
-                        Last Name
-                      </label>
-                      <input
-                        className="w-full rounded-lg border border-gray-300 px-4 pb-3 pt-3"
-                        placeholder="Last Name"
-                        id="lastName"
-                        type="text"
-                        {...formik.getFieldProps("lastName")}
-                        disabled={submitted}
-                      />
-                      {formik.touched.lastName && formik.errors.lastName ? (
-                        <div className="text-red-500">
-                          {formik.errors.lastName}
-                        </div>
-                      ) : null}
-                    </div>
-                    <div className="email mt-5">
-                      <label htmlFor="email" className="font-medium">
-                        Email Address
-                      </label>
-                      <input
-                        className="w-full rounded-lg border border-gray-300 px-4 pb-3 pt-3"
-                        placeholder="Email"
-                        id="email"
-                        type="email"
-                        {...formik.getFieldProps("email")}
-                        disabled={submitted}
-                      />
-                      {formik.touched.email && formik.errors.email ? (
-                        <div className="text-red-500">
-                          {formik.errors.email}
-                        </div>
-                      ) : null}
-                    </div>
-                    <button
-                      type="submit"
-                      className={`mt-5 w-full rounded-lg bg-gradient-to-r from-[#bb547d] via-[#9b5ba7] to-[#815fc8] py-2 font-normal text-white transition duration-300 hover:bg-[#e26178] ${
-                        !formik.isValid || !formik.dirty
-                          ? "cursor-not-allowed opacity-50"
-                          : ""
-                      }`}
-                      disabled={!formik.isValid || !formik.dirty || loading}
-                    >
-                      {loading ? <Preloader /> : <span>SIGN UP</span>}
-                    </button>
-                    {Error && <div className="mt-4 text-red-500">{Error}</div>}
-                  </form>
-                )}
-              </form>
+   <>
+    <head>
+    <title>Signup</title>
+    <meta
+          name="description"
+          content={
+           "Sign Up to WHP."
+          }
+        />
+    </head>
+    <div className="register-block py-10 md:py-20">
+      <div className="container">
+        <div className="flex justify-center max-md:flex-col">
+          <div className="border-line md:pr-[40px] lg:pr-[60px]">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="heading4 text-center text-[#e26178]">
+              <AuthAnimation />
+              <h1 className="mb-5 text-center text-lg font-normal text-[#E26178]">
+                SIGNUP TO WHP
+              </h1>
             </div>
+
+            {!isOtpVerified && (
+              <div className="flex flex-col items-center ">
+                <PhoneInput
+                  defaultCountry="in"
+                  value={phoneNumber}
+                  inputClassName="border h-full w-[250px] rounded-xl"
+                  placeholder="Enter your mobile number"
+                  onChange={handlePhoneChange}
+                />
+                <div className="block-button mt-4 md:mt-7 w-full">
+                  <div className="">
+                    <OtpVerification
+                      phoneNumber={phoneNumber}
+                      formikValues={formik.values}
+                      onSubmit={formik.handleSubmit}
+                      isRegisterPage={false}
+                      onOtpVerified={() => setIsOtpVerified(true)}
+                      errorMessage=""
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+
+
+            {isOtpVerified && (
+              <form onSubmit={formik.handleSubmit} className="mt-4 md:mt-7">
+                <div className="firstname">
+                  <label htmlFor="firstName" className="font-medium">
+                    First Name
+                  </label>
+                  <input
+                    className="w-full rounded-lg border border-gray-300 px-4 pb-3 pt-3"
+                    placeholder="First Name"
+                    id="firstName"
+                    type="text"
+                    {...formik.getFieldProps("firstName")}
+                    disabled={submitted}
+                  />
+                  {formik.touched.firstName && formik.errors.firstName ? (
+                    <div className="text-red-500">
+                      {formik.errors.firstName}
+                    </div>
+                  ) : null}
+                </div>
+                <div className="lastname mt-5">
+                  <label htmlFor="lastName" className="font-medium">
+                    Last Name
+                  </label>
+                  <input
+                    className="w-full rounded-lg border border-gray-300 px-4 pb-3 pt-3"
+                    placeholder="Last Name"
+                    id="lastName"
+                    type="text"
+                    {...formik.getFieldProps("lastName")}
+                    disabled={submitted}
+                  />
+                  {formik.touched.lastName && formik.errors.lastName ? (
+                    <div className="text-red-500">{formik.errors.lastName}</div>
+                  ) : null}
+                </div>
+                <div className="email mt-5">
+                  <label htmlFor="email" className="font-medium">
+                    Email Address
+                  </label>
+                  <input
+                    className="w-full rounded-lg border border-gray-300 px-4 pb-3 pt-3"
+                    placeholder="Email"
+                    id="email"
+                    type="email"
+                    {...formik.getFieldProps("email")}
+                    disabled={submitted}
+                  />
+                  {formik.touched.email && formik.errors.email ? (
+                    <div className="text-red-500">{formik.errors.email}</div>
+                  ) : null}
+                </div>
+                <button
+                  type="submit"
+                  className="mt-5 w-full rounded-lg bg-gradient-to-r from-[#bb547d] via-[#9b5ba7] to-[#815fc8] py-2 font-normal text-white transition duration-300 hover:bg-[#e26178]"
+                  disabled={loading}
+                >
+                  {loading ? <Preloader /> : <span>SIGN UP</span>}
+                </button>
+                {Error && <div className="mt-4 text-red-500">{Error}</div>}
+              </form>
+            )}
+            </form>
           </div>
         </div>
       </div>
-    </>
+    </div>
+   </>
   );
 };
 

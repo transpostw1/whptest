540413b92fad0d615ec83a790c6d9d506b026657
raamizz <rameezebrahim@transpost.co>
2025-01-30 11:08:01 +0530

    Image test

--- src/components/Home1/Category.tsx
+++ src/components/Home1/Category.tsx
@@ -55,12 +55,12 @@ const Category = () => {
                 }}
               >
                 <div className="effect14 cursor-pointer">
-                  <img
+                  <Image
                     src={category.parentImg}
-                    alt={`category images ${category.name}`}
+                    alt={category.name}
                     width={400}
                     height={400}
-                    // unoptimized
+                    unoptimized
                   />
                 </div>
               </Link>

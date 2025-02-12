eeb384550047caea8b3735d553c66a286fb397a9
raamizz <rameezebrahim@transpost.co>
2025-02-12 12:33:07 +0530

    Try on and other banners.

--- src/components/Product/Productgraphql.tsx
+++ src/components/Product/Productgraphql.tsx
@@ -339,16 +339,16 @@ const Product: React.FC<ProductProps> = ({ data, skuList }) => {
                           loadTryOnButton(data.SKU, data.productId)
                         }
                       >
-                        <div className="flex items-center justify-between text-[#e26178] hover:text-white">
+                        <div className="flex items-center justify-between text-[#e26178] hover:text-white py-1">
                           <IoCameraOutline />
-                          <p className="ps-1 text-sm">Try ON</p>
+                          {/* <p className="ps-1 text-sm">Try ON</p> */}
                         </div>
                       </div>
                     )}
                     {skuList?.includes(data.SKU) && isMobile && (
                       <div
                         id={`product-form-${data.productId}`}
-                        className="try_on absolute right-1 top-1 z-0 float-right flex justify-between rounded-lg border border-[#e26178] px-2 text-center hover:bg-[#e26178] hover:text-white"
+                        className="try_on absolute right-1 top-1 z-0 float-right flex justify-between  border border-[#e26178] px-2 text-center hover:bg-[#e26178] hover:text-white"
                         onClick={() =>
                           loadTryOnButton(data.SKU, data.productId)
                         }
@@ -359,7 +359,7 @@ const Product: React.FC<ProductProps> = ({ data, skuList }) => {
                       </div>
                     )}
                     {data.discountValue && (
-                      <div className="try_on absolute left-1 top-1 z-0 float-right flex justify-between border border-[#e26178] px-2 text-center text-xs hover:bg-[#e26178] hover:text-white">
+                      <div className="try_on absolute left-1 top-1 z-0 float-right flex justify-between border px-2 py-1 text-center text-xs bg-[#e26178] text-white">
                         {data.discountValue}% on making charges
                       </div>
                     )}

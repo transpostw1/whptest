8df64f8f26b4dc2f743a4b211dc388681f27e84d
raamizz <rameezebrahim@transpost.co>
2025-03-18 09:55:40 +0530

    Variants update

--- src/app/checkout/CartItem.tsx
+++ src/app/checkout/CartItem.tsx
@@ -65,7 +65,6 @@ const CartItem: React.FC<CartItemProps> = ({ product }) => {
 
   useEffect(() => {
     if (product) {
-      console.log(product, "huuuuuuuuu");
       const isMakeToOrder =
         product.makeToOrder === 1 || product.makeToOrder === true;
       if (

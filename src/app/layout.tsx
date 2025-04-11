21b1de746e2663187edc4aaa6470fc2d6ea494a3
raamizz <rameezebrahim@transpost.co>
2025-04-11 14:46:57 +0530

    Scripts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,11 +76,19 @@ export default function RootLayout({
       <html lang="en">
         {/* <Suspense> */}
         <head>
-          <Script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5T2F5CF');</Script>
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5T2F5CF');
+    `,
+            }}
+          />
           <Script
             id="organization-schema"
             type="application/ld+json"
@@ -89,7 +97,6 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               __html: JSON.stringify(organizationSchema),
             }}
           />
-
 
           <Script id="hotjar" strategy="afterInteractive">
             {`
@@ -112,7 +119,7 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           <Script id="google-analytics" strategy="afterInteractive">
             {`
     window.dataLayer = window.dataLayer || [];
-                   function gtag(){dataLayer.push(arguments);}      
+    function gtag(){dataLayer.push(arguments);}      
     gtag('js', new Date());
     gtag('config', 'G-KS3DVFD5ZW', {
       send_page_view: true,
@@ -138,8 +145,14 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           />
         </head>
         <body className={instrument.className}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5T2F5CF"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5T2F5CF"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
           <UserTracking />
           <TopNavOne textColor="text-white" />
           <NavTwo props="style-three bg-white" />

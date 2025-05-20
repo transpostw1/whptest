ba09ad82d18127ec5ccbd4614d3b535169129073
raamizz <rameezebrahim@transpost.co>
2025-05-20 12:53:59 +0530

    Separeted shopbreadcrumb test

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import ShopBreadCrumb1 from "@/components/Shop/ShopBreadCrumb1";
+import ShopBreadCrumb1 from "@/components/Shop/ShopBreadCrumb1/index";
 
 export default function BreadCrumb1() {
   return (

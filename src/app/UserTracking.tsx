04852bcf49e3e30c8b6faaa97cd71a3dadd5c4da
raamizz <rameezebrahim@transpost.co>
2025-05-27 17:03:52 +0530

    Google tag testing

--- src/app/UserTracking.tsx
+++ src/app/UserTracking.tsx
@@ -1,9 +1,22 @@
 "use client";
 import useUserTracking from "@/hooks/useUserTracking";
 import React, { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const UserTracking = () => {
   useUserTracking();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    // Track page views when route changes
+    if (typeof window !== "undefined" && window.gtag) {
+      window.gtag("config", "G-KS3DVFD5ZW", {
+        page_path: pathname + searchParams.toString(),
+      });
+    }
+  }, [pathname, searchParams]);
+
   return <></>;
 };
 
